af077fbe5080c29ff3b1aa42903a8bc43edf96e9
adrianpisabrrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-01-10 19:29:08 +0100

    Practica con excepciones añadidas

--- Practica/Practica.py
+++ Practica/Practica.py
@@ -4,25 +4,22 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-#lista de usuarios
+# lista de usuarios
 usuarios = [
     {
         'nombre': "adrian",
-        'edad': "23",
         'password': "12345Abcde",
         'email': "adrian.pisabarro.garcia@yahoo.es",
         'departamento': "Desarrollo"
     },
     {
         'nombre': "roberto",
-        'edad': "24",
         'password': "12345Abcde",
         'email': "roberto@yahoo.es",
         'departamento': "Sistemas"
     },
     {
         'nombre': "pepe",
-        'edad': "54",
         'password': "12345Abcde",
         'email': "pepe@yahoo.es",
         'departamento': "Compras"
@@ -30,7 +27,7 @@ usuarios = [
 ]
 
 
-#ejercicio 1
+# ejercicio 1
 def comprobar_usuario_existente(nombre):
     for usuario in usuarios:
         if usuario['nombre'] == nombre:
@@ -38,22 +35,47 @@ def comprobar_usuario_existente(nombre):
 
     return False
 
-#ejercicio 2
+
+def escribir_nombre():
+
+    correcto = False
+    while not correcto:
+        nombre = input("Escibe el nombre: ")
+
+        if nombre == "":
+            print("No has introducido correctamente el nombre")
+        else:
+            correcto = True
+
+    return nombre
+
+
+# ejercicio 2
 def longitud_password():
-    longitud = input("Escribe el número de caracteres que quieres que contenga la contraseña: ")
-    return int(longitud)
 
-#crea una pantalla para escoger: si o no.
+    correcto = False
+    while not correcto:
+        longitud = input("Escribe el número de caracteres que quieres que contenga la contraseña: ")
+
+        if longitud.isdigit():
+            return int(longitud)
+        else:
+            print("No has introducido un número, vuelve a intentarlo.")
+
+
+
+# crea una pantalla para escoger: si o no.
 def si_no_pantalla(texto):
-    opcion = input(f"¿Quieres incluir {texto} en la contraseña? Escribe 's' o 'n': ").lower()
+    correcto = False
+    while not correcto:
+        opcion = input(f"¿Quieres incluir {texto} en la contraseña? Escribe 's' o 'n': ").lower()
 
-    if opcion == "s" or opcion == "n":
-        return opcion
-    else:
-        print("Opcion mal escogida.")
-        si_no_pantalla(texto)
+        if opcion == "s" or opcion == "n":
+            return opcion
+        else:
+            print("Opcion mal escogida.")
 
-#ejercicio 3
+# ejercicio 3
 def fortaleza_password(data):
     print("""
     Criterios para la contraseña:
@@ -72,12 +94,20 @@ def fortaleza_password(data):
     else:
         print("FORTALEZA DÉBIL")
 
-#ejercicio 2
+
+# ejercicio 2
 def generar_password():
+    ninguna_opcion = True
     longitud = longitud_password()
-    minusculas = si_no_pantalla("minusculas")
-    mayusculas = si_no_pantalla("mayusculas")
-    simbolos = si_no_pantalla("simbolos")
+    while ninguna_opcion:
+        minusculas = si_no_pantalla("minusculas")
+        mayusculas = si_no_pantalla("mayusculas")
+        simbolos = si_no_pantalla("simbolos")
+
+        if minusculas == "n" and mayusculas == "n" and simbolos == "n":
+            print("No has introducido ninguna opción, vuelve a intentarlo. Te preguntaremos de nuevo...")
+        else:
+            ninguna_opcion = False
 
     lista_minusculas = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r",
                         "s",
@@ -96,7 +126,8 @@ def generar_password():
         todas.extend(lista_mayusculas)
     if simbolos == "s":
         todas.extend(lista_simbolos)
-    #Vamos a mezclar el array definitivo en el que hemos juntado todo lo que necesitamos
+
+    # Vamos a mezclar el array definitivo en el que hemos juntado todo lo que necesitamos
     random.shuffle(todas)
 
     password = ""
@@ -114,55 +145,60 @@ def generar_password():
         'password': password
     }
 
-    '''Ejercicio 3: Muestra la fortaleza de la contraseña'''
+    #Ejercicio 3: Muestra la fortaleza de la contraseña
     fortaleza_password(data)
 
     return password
 
-#ejercicio 4
+
+# ejercicio 4
 def escoger_departamento():
-    '''lista de departamentos'''
+    #lista de departamentos
     DEPARTAMENTOS = ['Compras', 'Ventas', 'Marketing', "Sistemas", "Desarrollo"]
 
-    print("Escoge un número de departamento: ")
-    for numero in range(len(DEPARTAMENTOS)):
-        print((str(numero + 1) + " .- " + DEPARTAMENTOS[numero]))
-    departamento_escogido = int(input())-1
+    correcto = False
+    while not correcto:
+        print("Escoge un número de departamento: ")
+        for numero in range(len(DEPARTAMENTOS)):
+            print((str(numero + 1) + " .- " + DEPARTAMENTOS[numero]))
+        departamento_escogido = int(input()) - 1
 
-    if departamento_escogido < len(DEPARTAMENTOS):
-        return str(DEPARTAMENTOS[departamento_escogido])
-    else:
-        print("Has introducido mal el numero de departamento, vuélvelo a introducir:")
-        escoger_departamento()
+        if departamento_escogido < len(DEPARTAMENTOS) and departamento_escogido >= 0:
+            return str(DEPARTAMENTOS[departamento_escogido])
+        else:
+            print("Has introducido mal el número de departamento, vuélvelo a introducir:")
 
-#ejercicio 5
+# ejercicio 5
 def introducir_email_valido():
-    email = input("Introduce un mail válido: ")
-    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    correcto = False
+    while not correcto:
+        email = input("Introduce un mail válido: ")
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
-    if (re.fullmatch(regex, email)):
-        return email
-    else:
-        print("Email invalido, vuelve a introducir un mail valido.")
-        introducir_email_valido()
+        if (re.fullmatch(regex, email)):
+            return email
+        else:
+            print("Email invalido, vuelve a introducir un mail valido.")
 
-#ejercicio 6
+
+# ejercicio 6
 def imprimir_usuario_concreto(usuario):
-    texto = ""
-    texto = texto + "Nombre: " + usuario['nombre'] + " - "
-    texto = texto + "Edad: " + usuario['edad'] + " - "
+    texto = "Nombre: " + usuario['nombre'] + " - "
+    texto = texto + "Contraseña: " + usuario['password'] + " - "
     texto = texto + "Email: " + usuario['email'] + " - "
     texto = texto + "Departamento: " + usuario['departamento']
+
     return texto
 
-#ejercicio 7
-def envio_email_registro(usuario):
-    #message = "Subject: Nuevo registro de usuario: " + imprimir_usuario_concreto(usuario)
 
+# ejercicio 7
+def envio_email_registro(usuario):
+    #datos: quien envia, quien recibe y contraseña de quien envía
     sender_email = "developersweapp@gmail.com"
     receiver_email = usuario['email']
     password = "12345Abcde"
 
+    #hacemos el mensaje
     message = MIMEMultipart("alternative")
     message["Subject"] = "Nuevo usuario registrado"
     message["From"] = sender_email
@@ -171,78 +207,70 @@ def envio_email_registro(usuario):
     html = """\
     <html>
       <body>
-      <h2>¡Felicidades por registrarte """ + usuario['nombre'] +"""!</h2>
+      <h2>¡Felicidades por registrarte """ + usuario['nombre'] + """!</h2>
         <p>
-        """ + user_print_email_data +"""
+        """ + user_print_email_data + """
         </p>
       </body>
     </html>
     """
 
-    # Turn these into plain/html MIMEText objects
+    # Convertimos el mail a formato html
     part2 = MIMEText(html, "html")
 
-    # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
+    # Añadimos el html al mensaje
     message.attach(part2)
 
-    # Create secure connection with server and send email
+    # Enviamos el correo electrónico
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(
-            sender_email, receiver_email, message.as_string()
-        )
+        server.sendmail(sender_email, receiver_email, message.as_string())
 
-#crea usuarios, imprime el usuario creado, añade usuarios al array y envia un correo
+
+# crea usuarios, imprime el usuario creado, añade usuarios al array y envia un correo
 def crear_usuario():
 
-    nombre = input("Escibe el nombre: ")
-    '''ejercicio 1: Compruebo si el usuario esta creado o no:'''
-    if comprobar_usuario_existente(nombre):
-        print("El usuario ya existe, crea otro usuario")
-        crear_usuario()
-    edad = input("Escibe la edad: ")
-    '''ejericio 2: generador de contraseñas'''
+    repetir_usuario = True
+    while repetir_usuario:
+        nombre = escribir_nombre()
+        # ejercicio 1: Compruebo si el usuario esta creado o no:
+        if comprobar_usuario_existente(nombre):
+            print("El usuario ya existe, crea otro usuario")
+        else:
+            repetir_usuario = False
+
+    # ejericio 2: generador de contraseñas
     password = generar_password()
 
-    '''ejercicio 4: escoger departamento'''
+    # ejercicio 4: escoger departamento
     departamento = escoger_departamento()
 
-    '''ejercicio 5: introducir un mail válido'''
+    # ejercicio 5: introducir un mail válido
     email = introducir_email_valido()
-
 
     usuario = {
         'nombre': nombre,
-        'edad': edad,
         'password': password,
         'email': email,
         'departamento': departamento
     }
 
-    #ejercicio 6
+    # ejercicio 6
     print(imprimir_usuario_concreto(usuario))
     usuarios.append(usuario)
     print(f"Ahora mismo hay {len(usuarios)} usuario(s) creado(s).")
 
-    #ejercicio 7
+    # ejercicio 7
     envio_email_registro(usuario)
 
-#ejercicio 6
-def listar_usuarios():
-    print("**********************************")
-    for usuario in usuarios:
-        texto = ""
-        texto = texto + "Nombre: " + usuario['nombre'] + " - "
-        texto = texto + "Edad: " + usuario['edad'] + " - "
-        """ Se puede eliminar la password para no mostrarla"""
-        texto = texto + "Password: " + usuario['password'] + " - "
-        texto = texto + "Email: " + usuario['email'] + " - "
-        texto = texto + "Departamento: " + usuario['departamento']
-        print(texto)
 
-    print("**********************************")
+# ejercicio 6
+def listar_usuarios():
+    print("**************************")
+    for usuario in usuarios:
+        print(imprimir_usuario_concreto(usuario))
+
 
 # __main__: programa principal
 repetir = True
@@ -254,7 +282,6 @@ while repetir:
     3.- Salir
     """)
 
-    print("**************************")
     if opcion == "1":
         crear_usuario()
     elif opcion == "2":
@@ -263,4 +290,3 @@ while repetir:
         repetir = False
     else:
         print("Has introducido un número incorrecto, vuelve a intentarlo: ")
-
